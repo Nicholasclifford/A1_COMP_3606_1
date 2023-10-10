de82a0e1192d58e4bd19a87a5816047a4ff7bc30
nicholas.clifford <nicholas.clifford@my.uwi.edu>
2023-10-10 14:58:45 -0400

    New Start

--- app/src/main/java/com/example/a1_comp_3606/MainActivity.kt
+++ app/src/main/java/com/example/a1_comp_3606/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.a1_comp_3606
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
-import java.time.Duration
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
+    private val requestCode = 1234
+    private var hasPermissions = false
+
 
 
 
@@ -17,22 +26,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button:Button=findViewById(R.id.Wifibutton)
-        button.setOnClickListener { this }
 
-        val SellerInfoLayout: LinearLayout=findViewById(R.id.Listing_Info)
+
+        val button:Button=findViewById(R.id.Wifibutton)
+        button.setOnClickListener {
+            val Wifiviewmenu=LayoutInflater.from(this).inflate(R.layout.wifimenu,null)
+
+            val WifiBuilder=AlertDialog.Builder(this)
+                .setView(Wifiviewmenu)
+                .setTitle("Menu for Wifi")
+
+            WifiBuilder.show()
+
+
+
+
+
+
+            Toast.makeText(this,"button 1 clicked", Toast.LENGTH_SHORT).show()
+
+            //this is code to change the visibility of  The other section of the app
+            val one: LinearLayout =findViewById(R.id.Listing_Info)
+            one.setVisibility(View.VISIBLE)
+
+            val two: LinearLayout =findViewById(R.id.Marketplace)
+            if(two.visibility ==LinearLayout.GONE)
+                two.setVisibility(View.VISIBLE)
+            else
+                two.setVisibility(View.GONE)
+        }
+
+
+
+
+
+
+
 
     }
 
 
 
-    fun buttonInfoPop(v:View)
-     {
-         Toast.makeText(this, "Button pressed",Toast.LENGTH_LONG).show()
-
-
-     }
-
-
-
 }
+
+
---
+++ app/src/main/java/com/example/a1_comp_3606/WifiDirectManager.kt
@@ -0,0 +1,14 @@
+package com.example.a1_comp_3606
+
+import android.net.wifi.p2p.WifiP2pManager
+
+class WifiDirectManager {
+
+
+
+
+
+
+
+
+}
